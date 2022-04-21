ec08112710fff649b2b5e7589083a174e4bbf54b
radboy2004 <402487515@qq.com>
2022-04-22 00:31:11 +0800

    set alias path, scss global var

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { resolve } from 'path'
+const pathResolve = (file: string): string => {
+  return resolve(__dirname, file)
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  resolve: {
+    alias: {
+      '@': pathResolve('src'),
+      '@pkg': pathResolve('packages'),
+      '@comps': pathResolve('src/components'),
+      '@assets': pathResolve('src/assets')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@pkg/scss/var.scss";`
+      }
+    }
+  },
+  plugins: [vue()],
+  server: {
+    open: true
+  }
 })
